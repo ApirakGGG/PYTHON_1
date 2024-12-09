b9b1af696a87dbe6b79edf24211bf91fbc98f68f
ApirakGGG <naratah012345@gmail.com>
2024-12-09 20:07:13 +0700

    python files

---
+++ MusicPlayer_App/Music_App.py
@@ -0,0 +1,8 @@
+import os 
+from tkinter import Tk, Frame,Listbox,Button,filedialog,Label
+import pygame as pg
+
+# // Class Musiv_Player
+class Music_Player_App :
+    def __init__(self, root):
+        pass
